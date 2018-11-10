3a1adb1c401df8f514941fe6e0c9f272ddfe52c8
fangxiaorong <fangxiaorong@daojia-inc.com>
2018-11-10 23:23:35 +0800

    fix

--- server/backends/server.py
+++ server/backends/server.py
@@ -5,6 +5,7 @@
 
 
 import os
+from datetime import datetime
 
 import json
 import sqlite3
@@ -406,11 +407,11 @@ class ManagerGetLocations(web.RequestHandler):
                 for location in locations:
                     update_time = location.get('pos_update_time')
                     if update_time:
-                        update_time = datetime.datetime.strptime(update_time, '%Y-%m-%dT%H:%M:%S.%f')
+                        update_time = datetime.strptime(update_time, '%Y-%m-%dT%H:%M:%S.%f')
                     else:
-                        update_time = datetime.datetime(2000, 1, 1)
+                        update_time = datetime(2000, 1, 1)
                     if location.get('latitude') and location.get('longitude'):
-                        if (datetime.datetime.now() - update_time).total_score < 300:
+                        if (datetime.now() - update_time).total_score < 300:
                             location.update({'state': 1})
                         else:
                             location.update({'state': 2})
