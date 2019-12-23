1747020f1ef86fc28a044809dfc4353b4dfa69e0
fangxiaorong <fangxiaorong@daojia-inc.com>
2019-12-23 20:42:30 +0800

    fix

--- web/src/pages/Login.vue
+++ web/src/pages/Login.vue
@@ -7,7 +7,7 @@
             <v-card class="elevation-1 pa-3">
               <v-card-text>
                 <div class="layout column align-center">
-                  <img src="/static/m.png" alt="Vue Material Admin" width="120" height="120">
+                  <img src="/static/logo.png" alt="Vue Material Admin" width="120" height="120">
                   <h1 class="flex my-4 primary--text">用户登陆</h1>
                   <div style="color: red;">{{ message }}</div>
                 </div>                
