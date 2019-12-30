68aba229a719e452f57efc0c45fc1e1a63e13801
fangxiaorong <fangxiaorong@daojia-inc.com>
2019-12-30 21:54:47 +0800

    fix

--- server/backends/__init__.py
+++ server/backends/__init__.py
@@ -423,7 +423,7 @@ class ExamUser(BaseTable):
             for info in zip(user_array, warrings):
                 pos_info = info[0]
                 if info[1] is not None:
-                    pos_info.update({'state': 4})
+                    pos_info.update({'state': 4, 'warring_time': info[1]})
                 else:
                     tmp_time = float(pos_info.get('create_time')) if pos_info.get('create_time') else 0
                     if current_time - tmp_time < 300:
@@ -558,7 +558,7 @@ class UserRecord(BaseTable):
     def add_warring(self, user_id, latitude, longitude):
         result = self.add_record(user_id, latitude, longitude)
         if result is None:
-            r_conn.setex(self.warring_prefix + str(user_id), '%f,%f' % (longitude, latitude), 300)
+            r_conn.setex(self.warring_prefix + str(user_id), '%f' % datetime.now().timestamp(), 300)
         return None
 
 
